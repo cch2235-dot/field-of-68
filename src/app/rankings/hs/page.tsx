b0ea2dc81884ed956fe617c5a7456f024f66497d
cch2235-dot <cch2235@gmail.com>
2026-07-26 23:03:32 -0400

    Update page.tsx

--- src/app/rankings/hs/page.tsx
+++ src/app/rankings/hs/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { useState } from 'react';
-import hsData from '../../../data/hs-rankings.json';
+import hsData from '../../../../data/hs-rankings.json';
 
 type Prospect = {
   rank: number;
